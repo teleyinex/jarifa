30c5b724a51ac38314dc49ffb76846f1359f6ac0
Daniel Lombraña González <daniellg@unex.es>
2008-07-21 22:16:38 +0000

    First version where it works login and logout and showing a starting page.
    git-svn-id: https://forja.unex.es/svn/ogm/trunk@4 745f72ad-301c-453d-9255-a92439f6a4c4

--- index.php
+++ index.php
@@ -6,44 +6,24 @@ require_once("controller/controller.inc");
 require_once("inc/lang.inc");
 
 $ctr = new controller();
-// If $_POST is empty, OGM should show the login screen
-if (empty($_POST) and (!isset($_SESSION['userid'])))
+
+if (!isset($_SESSION['userid']))
 {
-    $view= new login($language);
-    $view->form();
+    $ctr->view("login");
 }
 else
 {
-    switch($_POST['action'])
+    switch ($_GET['action'])
     {
-    case 'authenticate':
-    {
-        if ($ctr->authenticate($_POST['userid'],$_POST['password']))
-        {
-             printf($_SESSION['userid']);
-             print_r($_SESSION);
-        }
-        else
-        {
-            printf(gettext("ERROR, the username or password is incorrect.")); 
-        }
-    }
-    default:
-    {
-        // If the user has log in, it can perform different accions based on this role
-        if (isset($_SESSION['user']))
-            switch ($_SESSION['role'])
-            {
-                case 'supplier':
-                {
-                    printf("SUPPLIER");
-                }
-                case 'allocator':
-                {
-                    printf("ALLOCATOR");
-                }
-            }
-    }
+        case 'start':
+            $ctr->view('start');
+            break;
+
+        case 'logout':
+            $ctr->logout();
+            break;
+
+    
     }
 }
 
