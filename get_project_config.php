d01083b99ea13b31f82d49166ec7c68b28fbc00c
Daniel Lombraña González <daniellg@unex.es>
2008-07-29 18:06:35 +0000

    The get_project_config.php file that any Account Manager needs.
    git-svn-id: https://forja.unex.es/svn/ogm/trunk@38 745f72ad-301c-453d-9255-a92439f6a4c4

---
+++ get_project_config.php
@@ -0,0 +1,11 @@
+<?php
+require_once("inc/xml.inc");
+xml_header();
+printf("
+<project_config>
+    <name>AM Server DANIEL</name>
+    <master_url>http://127.0.0.1/teleyinex/</master_url>
+    <min_passwd_length>1</min_passwd_length> 
+    <account_manager/> 
+</project_config>");
+?>
