3070040e75f87ed47133e7c6d80275e2214d4a9f
Daniel Lombraña González <daniellg@unex.es>
2008-07-31 18:16:16 +0000

    (refs #5), The account manager settings have been added to the configuration file.
    git-svn-id: https://forja.unex.es/svn/ogm/trunk@48 745f72ad-301c-453d-9255-a92439f6a4c4

--- get_project_config.php
+++ get_project_config.php
@@ -1,23 +1,23 @@
 <?php
-$dom = new DOMDocument('1.0', 'iso-8859-1');
-$dom->formatOutput = true;
 
+$conf = simplexml_load_file("conf/ogm.conf");
+
+$dom = new DOMDocument('1.0', 'UTF-8');
+$dom->formatOutput = true;
 $root = $dom->createElement('project_config');
 $dom->appendChild($root);
-$element = $dom->createElement('name','AM Server Daniel');
+$element = $dom->createElement('name',$conf->account_manager->name);
 $root->appendChild($element);
-$element = $dom->createElement('master_url','http://127.0.0.1/teleyinex/trunk</master_url>');
+$element = $dom->createElement('master_url',$conf->account_manager->master_url);
 $root->appendChild($element);
-$element = $dom->createElement('min_passwd_length','1');
+$element = $dom->createElement('min_passwd_length',$conf->account_manager->min_passwd_length);
 $root->appendChild($element);
 $element = $dom->createElement('account_manager');
 $root->appendChild($element);
+if ($conf->account_manager->usernames == '1')
+    {
+    $element = $dom->createElement('user_username');
+    $root->appendChild($element);
+    }
 echo $dom->saveXML();
-//<project_config>
-//    <name>AM Server DANIEL</name>
-//    <master_url>http://127.0.0.1/teleyinex/trunk</master_url>
-//    <min_passwd_length>1</min_passwd_length> 
-//    <account_manager/> 
-//    <user_username/>
-//</project_config>");
 ?>
