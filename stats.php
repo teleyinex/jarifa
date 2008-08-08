0d7a189821c13893252ffd7dbfe3a1573f427b74
Daniel Lombraña González <daniellg@unex.es>
2008-08-08 23:01:39 +0000

    Added license GNU Affero v3 except for db_conn.inc and libchart, closes #10.
    git-svn-id: https://forja.unex.es/svn/ogm/trunk@70 745f72ad-301c-453d-9255-a92439f6a4c4

--- stats.php
+++ stats.php
@@ -1,4 +1,23 @@
 <?php 
+/* 
+Copyright 2008 Daniel Lombraña González, David Anderson, Franisco Fernández de Vega
+
+This file is part of OGM.
+
+OGM is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OGM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with OGM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 require_once("inc/stats.inc");
 
 $st = new stats();
